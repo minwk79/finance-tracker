d81d3d4888bfc0b30fcbf2a43f99f76312992ee8
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-10 15:40:13 -0800

    User Model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    minLength: 4,
+    required: true
+  },
+  goal: {
+    type: Schema.Types.ObjectId,
+    ref: 'Goal'
+  },
+  monthlySpendings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'MonthlySpending'
+    }
+  ]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+
+module.exports = mongoose.model('User', userSchema);
