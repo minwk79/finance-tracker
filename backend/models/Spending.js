9bc37a2150381ecdf8a71a1682da7361080fb482
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-14 15:57:11 -0800

    spending model

---
+++ backend/models/Spending.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const spendingSchema = new Schema({
+  title: {
+    type: String
+  },
+  type: {
+    type: String,
+    enum: [ 
+      'housing', 
+      'transportation', 
+      'food', 
+      'insurance-health', 
+      'saving', 
+      'personal-spending'
+    ]
+  },
+  details: {
+    type: String
+  },
+  amount: {
+    type: Number
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+spendingSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+module.exports = mongoose.model('Spending', spendingSchema);
+
