9a70a3b61a8910257dab61fb6a90906d43eb55b8
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-23 12:23:55 -0800

    routing for main page and 404 page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  {path: 'main', component: MainPageComponent},
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
