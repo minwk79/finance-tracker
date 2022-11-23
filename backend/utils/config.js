ad354363eb90bbc0e1e2f91389b8069a78800391
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-23 13:09:20 -0800

    updated config for nodemailer email/password

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,14 +4,14 @@ const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const SALT = process.env.SALT;
 const JWT_KEY =process.env.JWT_KEY;
-// const EMAIL = process.env.EMAIL;
-// const PASSWORD = process.env.PASSWORD;
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
 
 module.exports = {
   PORT,
   MONGO_URI,
   SALT,
   JWT_KEY,
-  // EMAIL,
-  // PASSWORD
+  EMAIL,
+  PASSWORD
 };
