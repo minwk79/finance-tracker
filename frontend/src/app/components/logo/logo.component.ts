51d5aed95462011606da1deb2ccd10a1523c961b
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-23 12:24:51 -0800

    basic logo componet

---
+++ frontend/src/app/components/logo/logo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent implements OnInit {
+
+  title = 'myTracker';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
