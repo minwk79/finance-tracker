bdf01671064c1c67aad388fbd4e97cf5dcc86c33
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-26 14:50:02 -0800

    personal details model

---
+++ frontend/src/app/models/personal-details.spec.ts
@@ -0,0 +1,7 @@
+import { PersonalDetails } from './personal-details';
+
+describe('PersonalDetails', () => {
+  it('should create an instance', () => {
+    expect(new PersonalDetails()).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/models/personal-details.ts
@@ -0,0 +1,5 @@
+export class PersonalDetails {
+  username!: string;
+  email!: string;
+  password!: string;
+}
