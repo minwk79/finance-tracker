36cb488eb6f07c1fd694172fbde14a2a6305693b
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-28 16:13:18 -0800

    basic header component

---
+++ frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() showNavbar = false;
+  @Input() showSignin = false;
+  @Input() showSignout = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  gotoSignIn() {
+    this.router.navigateByUrl('/signin');
+  }
+
+}
