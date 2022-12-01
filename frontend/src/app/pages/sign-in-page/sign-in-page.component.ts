6dbbbefd92a27041d4bbd26b00050501fb29c9eb
minugrammer0725 <kwawkminoo@gmail.com>
2022-11-30 18:26:24 -0800

    sign in form: basic form with validation

--- frontend/src/app/pages/sign-in-page/sign-in-page.component.ts
+++ frontend/src/app/pages/sign-in-page/sign-in-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
+
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPageComponent implements OnInit {
 
+  signinForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  signin() {
+    console.log(this.signinForm.getRawValue());
+
+    this.signinForm.reset();
+
+    // TODO:
+    // on success, navigate to /home page + store returned data (user, including token)
+    // on failure, alert user with error message
+    // Create & use userService(signupService equivalent)
+  }
 }
