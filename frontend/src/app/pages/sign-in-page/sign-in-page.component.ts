e4b4825bdb0e8f610d49c7708cf26a177e9b80af
minugrammer0725 <kwawkminoo@gmail.com>
2022-12-01 16:13:57 -0800

    refactored sign in page: sign in user working with error and validation

--- frontend/src/app/pages/sign-in-page/sign-in-page.component.ts
+++ frontend/src/app/pages/sign-in-page/sign-in-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms'; 
 
+import { Router } from '@angular/router';
+import { SignInService } from 'src/app/services/sign-in.service';
+
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
@@ -9,24 +12,45 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignInPageComponent implements OnInit {
 
+  hidePW = true;
+  error: boolean | null = null;
+
   signinForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required, Validators.minLength(4)])
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    rememberMe: new FormControl(false)
   })
 
-  constructor() { }
+  constructor(
+    private signinService: SignInService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   signin() {
-    console.log(this.signinForm.getRawValue());
-
+    let body = this.signinForm.getRawValue();
+    const observer = {
+      next: (user: any) => {
+        window.localStorage.setItem('user', JSON.stringify(user));  // OR, just save the token instead of user...
+        // redirect to /home page
+        this.router.navigateByUrl('/home');
+      },
+      error: (err: Error) => {
+        // set error to true and set back to false.. 
+        this.error = true;
+        setTimeout(() => {
+          this.error = false;
+        }, 3000);
+      }
+    }
+    this.signinService.signin(body).subscribe(observer);
     this.signinForm.reset();
-
-    // TODO:
-    // on success, navigate to /home page + store returned data (user, including token)
-    // on failure, alert user with error message
-    // Create & use userService(signupService equivalent)
   }
+
+  toggleRememberMe() {
+    this.signinForm.getRawValue().rememberMe = !this.signinForm.getRawValue().rememberMe;
+  }
+
+
 }
